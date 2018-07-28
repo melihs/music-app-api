73a08268a1c16faf57b93f0d2963749fdfc18b00
Öner Çiller <onerciller@gmail.com>
2018-07-28 20:50:25 +0300

    fix typo

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,12 +8,12 @@ use App\Category;
 class CategoryController extends BaseController
 {
     public function index()
-    {
+    {        
         try {
-            $category = Category::all();
-            $this->sendResponse($category, 200, null);
+            $category = Category::all();            
+            return $this->sendResponse($category, 200, null);
         } catch (\Exception $e) {
-            $this->sendResponse(null, 500, $e->getMessage());
+            return $this->sendResponse(null, 500, $e->getMessage());
         }
     }
 }
