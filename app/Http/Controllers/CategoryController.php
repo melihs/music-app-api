900eb9017babc47d1e3758b62fba59110bca7f5b
Öner Çiller <onerciller@gmail.com>
2018-07-28 16:46:20 +0300

    added index for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends BaseController
 {
     public function index()
     {
-        
+        try {
+            $category = Category::all();
+            $this->sendResponse($category, 200, null);
+        } catch (\Exception $e) {
+            $this->sendResponse(null, 500, $e->getMessage());
+        }
     }
 }
