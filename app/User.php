6e42b9a4ab1829881e5b4995a7d4692fc7d02fd7
Öner Çiller <onerciller@gmail.com>
2018-07-28 17:08:44 +0300

    user relations and favorites

--- app/User.php
+++ app/User.php
@@ -28,4 +28,34 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    /**
+     * favorites list that the user has favorite songs
+     */
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'user_id', 'song_id');
+    }
+    
+
+    /**
+     * addFavorite adds the user's favorite song
+     */
+    public function addFavorite($user_id)
+    {
+        if (!$this->favorites->contains($user_id)) {
+            $this->favorites()->attach($user_id);
+        }
+    }
+    
+    /**
+     * removeFavorite removes the user's favorite song
+     */
+    public function removeFavorite($user_id)
+    {
+        if (!$this->favorites->contains($user_id)) {
+            $this->favorites()->detach($user_id);
+        }
+    }
 }
