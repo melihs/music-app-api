e5967e8e872a165b189b51fa590d58e42c0fb9a5
Öner Çiller <onerciller@gmail.com>
2018-07-28 17:06:33 +0300

    song list is added

--- app/Http/Controllers/SongCongroller.php
+++ app/Http/Controllers/SongCongroller.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Song;
 
 class SongCongroller extends BaseController
 {
-    //
+    public function index()
+    {
+        try {
+            $song = Song::all();
+            $this->sendResponse($song, 200, null);
+        } catch (\Exception $e) {
+            $this->sendResponse(null, 500, $e->getMessage());
+        }
+    }
 }
